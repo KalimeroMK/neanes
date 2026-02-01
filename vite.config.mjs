422c5194fed4ffb799ca53d789d38ad634bc7c3f
KalimeroMK <zbogoevski@gmail.com>
2026-02-01 21:58:00 +0100

    Add macedonia3d.mk to Vite server allowedHosts

--- vite.config.mjs
+++ vite.config.mjs
@@ -32,137 +32,140 @@ export default defineConfig(({ command, mode }) => {
     plugins: [
       mode === 'web'
         ? VitePWA({
-            registerType: null, // We'll inject the service worker ourselves
-            workbox: {
-              maximumFileSizeToCacheInBytes: 3000000,
-            },
-            includeAssets: [
-              'favicon-32.png',
-              'favicon-16.png',
-              'apple-touch-icon.png',
-              'msapplication-icon.png',
-              'safari-pinned-tab.svg',
+          registerType: null, // We'll inject the service worker ourselves
+          workbox: {
+            maximumFileSizeToCacheInBytes: 3000000,
+          },
+          includeAssets: [
+            'favicon-32.png',
+            'favicon-16.png',
+            'apple-touch-icon.png',
+            'msapplication-icon.png',
+            'safari-pinned-tab.svg',
+          ],
+          manifest: {
+            name: 'Neanes',
+            short_name: 'Neanes',
+            description: 'A Byzantine Chant Scorewriter',
+            theme_color: '#052F43',
+            icons: [
+              {
+                src: 'assets/icons/icon-72x72.png',
+                sizes: '72x72',
+                type: 'image/png',
+                purpose: 'maskable any',
+              },
+              {
+                src: 'assets/icons/icon-96x96.png',
+                sizes: '96x96',
+                type: 'image/png',
+                purpose: 'maskable any',
+              },
+              {
+                src: 'assets/icons/icon-128x128.png',
+                sizes: '128x128',
+                type: 'image/png',
+                purpose: 'maskable any',
+              },
+              {
+                src: 'assets/icons/icon-144x144.png',
+                sizes: '144x144',
+                type: 'image/png',
+                purpose: 'maskable any',
+              },
+              {
+                src: 'assets/icons/icon-152x152.png',
+                sizes: '152x152',
+                type: 'image/png',
+                purpose: 'maskable any',
+              },
+              {
+                src: 'assets/icons/icon-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+                purpose: 'maskable any',
+              },
+              {
+                src: 'assets/icons/icon-384x384.png',
+                sizes: '384x384',
+                type: 'image/png',
+                purpose: 'maskable any',
+              },
+              {
+                src: 'assets/icons/icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable any',
+              },
             ],
-            manifest: {
-              name: 'Neanes',
-              short_name: 'Neanes',
-              description: 'A Byzantine Chant Scorewriter',
-              theme_color: '#052F43',
-              icons: [
-                {
-                  src: 'assets/icons/icon-72x72.png',
-                  sizes: '72x72',
-                  type: 'image/png',
-                  purpose: 'maskable any',
-                },
-                {
-                  src: 'assets/icons/icon-96x96.png',
-                  sizes: '96x96',
-                  type: 'image/png',
-                  purpose: 'maskable any',
-                },
-                {
-                  src: 'assets/icons/icon-128x128.png',
-                  sizes: '128x128',
-                  type: 'image/png',
-                  purpose: 'maskable any',
-                },
-                {
-                  src: 'assets/icons/icon-144x144.png',
-                  sizes: '144x144',
-                  type: 'image/png',
-                  purpose: 'maskable any',
-                },
-                {
-                  src: 'assets/icons/icon-152x152.png',
-                  sizes: '152x152',
-                  type: 'image/png',
-                  purpose: 'maskable any',
-                },
-                {
-                  src: 'assets/icons/icon-192x192.png',
-                  sizes: '192x192',
-                  type: 'image/png',
-                  purpose: 'maskable any',
-                },
-                {
-                  src: 'assets/icons/icon-384x384.png',
-                  sizes: '384x384',
-                  type: 'image/png',
-                  purpose: 'maskable any',
-                },
-                {
-                  src: 'assets/icons/icon-512x512.png',
-                  sizes: '512x512',
-                  type: 'image/png',
-                  purpose: 'maskable any',
-                },
-              ],
-            },
-          })
+          },
+        })
         : undefined,
       vue(),
       process.env.VITE_ENABLE_DEV_TOOLS === 'true' ? VueDevTools() : undefined,
       eslintPlugin(),
       !mode.includes('web')
         ? electron([
-            {
-              // Main-Process entry file of the Electron App.
-              entry: 'electron/main/index.ts',
-              onstart(options) {
-                if (process.env.VSCODE_DEBUG) {
-                  console.log(
+          {
+            // Main-Process entry file of the Electron App.
+            entry: 'electron/main/index.ts',
+            onstart(options) {
+              if (process.env.VSCODE_DEBUG) {
+                console.log(
                     /* For `.vscode/.debug.script.mjs` */ '[startup] Electron App',
-                  );
-                } else {
-                  options.startup();
-                }
-              },
-              vite: {
-                build: {
-                  sourcemap,
-                  minify: isBuild,
-                  outDir: 'dist-electron/main',
-                  rollupOptions: {
-                    external: Object.keys(
-                      'dependencies' in pkg ? pkg.dependencies : {},
-                    ),
-                  },
+                );
+              } else {
+                options.startup();
+              }
+            },
+            vite: {
+              build: {
+                sourcemap,
+                minify: isBuild,
+                outDir: 'dist-electron/main',
+                rollupOptions: {
+                  external: Object.keys(
+                    'dependencies' in pkg ? pkg.dependencies : {},
+                  ),
                 },
               },
             },
-            {
-              entry: 'electron/preload/index.ts',
-              onstart(options) {
-                // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
-                // instead of restarting the entire Electron App.
-                options.reload();
-              },
-              vite: {
-                build: {
-                  sourcemap: sourcemap ? 'inline' : undefined, // #332
-                  minify: isBuild,
-                  outDir: 'dist-electron/preload',
-                  rollupOptions: {
-                    external: Object.keys(
-                      'dependencies' in pkg ? pkg.dependencies : {},
-                    ),
-                  },
+          },
+          {
+            entry: 'electron/preload/index.ts',
+            onstart(options) {
+              // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
+              // instead of restarting the entire Electron App.
+              options.reload();
+            },
+            vite: {
+              build: {
+                sourcemap: sourcemap ? 'inline' : undefined, // #332
+                minify: isBuild,
+                outDir: 'dist-electron/preload',
+                rollupOptions: {
+                  external: Object.keys(
+                    'dependencies' in pkg ? pkg.dependencies : {},
+                  ),
                 },
               },
             },
-          ])
+          },
+        ])
         : undefined,
     ],
     server:
-      process.env.VSCODE_DEBUG &&
-      (() => {
-        const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL);
-        return {
-          host: url.hostname,
-          port: +url.port,
-        };
-      })(),
+      process.env.VSCODE_DEBUG
+        ? (() => {
+          const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL);
+          return {
+            host: url.hostname,
+            port: +url.port,
+          };
+        })()
+        : {
+          allowedHosts: ['macedonia3d.mk', 'localhost'],
+        },
     test: {
       globals: true,
     },
